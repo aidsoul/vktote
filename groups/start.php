93ffe92e77df69e58534caaaeae64bdb63ec8d74
aidsoul <work-aidsoul@outlook.com>
2022-07-17 13:20:49 +0300

    Error fix #2

--- groups/start.php
+++ groups/start.php
@@ -1,5 +1,24 @@
 <?php
+
+$start = microtime(true);
+$memory = memory_get_usage();
+
 $path = __DIR__.'/../';
 require_once $path.'vendor/autoload.php';
 require_once $path.'config.php';
 Vktote\Bot::start(__DIR__.'/'.$argv[1].'/config.ini');
+
+$memory = memory_get_usage() - $memory;
+$time = microtime(true) - $start;
+
+// Перевод в КБ, МБ.
+$i = 0;
+while (floor($memory / 1024) > 0) {
+	$i++;
+	$memory /= 1024;
+}
+ 
+$name = array('байт', 'КБ', 'МБ');
+$memory = round($memory, 2) . ' ' . $name[$i];
+ 
+echo $time . ' сек. / ' . $memory;
